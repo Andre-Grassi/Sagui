a43f98c84edf0c9ace664305fd7244318c472429
Andre Grassi <andregrassi.j@gmail.com>
2024-04-14 14:23:31 -0300

    [add] Pseudocódigo do teste em assembly

---
+++ test.asm
@@ -0,0 +1,11 @@
+# Inicializar registrador 0 --> fazer um movh e depois um movl
+# Inicializar registrador 1 --> fazer um movr do valor do 0
+# Testes da ULA e guardar tudo no 1
+# Store
+# Load
+# brzr dando certo
+# brzr dando errado
+# brzi dando certo
+# brzi dando errado
+# jr
+# ji
