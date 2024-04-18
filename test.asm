d342549950953eccffec5fc2739f2355e36784f2
Andre Grassi <andregrassi.j@gmail.com>
2024-04-17 23:10:44 -0300

    [add] Teste de subtrações

--- test.asm
+++ test.asm
@@ -1,3 +1,5 @@
+## mnemonico reg # operação --> # instrução em binário = instr. em hex.
+
 # Inicializa registrador 0 com o valor XXXXXXX não sei qual é o resultado
 sub $0, $0  # R[0] = R[0] - R[0] # Reseta o registrador 0 --> 10100000 = A0
 movh 7      # R[0] = {Imm + R[0](3:0)} = {0111 + 0000} = 01110000 = 112 --> 01110111 = 77
@@ -8,27 +10,34 @@ movl 7      # R[0] = {R[0](7:4) + Imm.} = {0000 + 0111} = 00000111 = 7 --> 10000
 
 # Inicializar registrador 1 --> fazer um movr do valor do 0
 movr $1, $0 # R[1] = R[0] = 7 --> 01100100 = 64
+movr $2, $1 # R[2] = R[1] = 7 --> 01101001 = 69
+movr $3, $2 # R[3] = R[2] = 7 --> 01101110 = 6E
+
 
 ######## ADD ########
 
 # Soma de positivo com positivo
-add $1, $1  # R[1] = R[1] + R[1] = 7 + 7 = 14 --> 10010101 = 95
+add $2, $1  # R[2] = R[2] + R[1] = 7 + 7 = 14 --> 10011001 = 99
 sub $0, $0  # R[0] = R[0] - R[0] # Reseta o registrador 0 --> 10100000 = A0
 movh 7      # R[0] = {Imm + R[0](3:0)} = {1111 + 0000} = 11110000 = -16 --> 01111111 = 7F
 
 # Soma de positivo com negativo
-add $1, $0 # R[1] = R[1] + R[0] = 14 + (-16) = -2 --> 10010100 = 94
+add $3, $0 # R[3] = R[3] + R[0] = 7 + (-16) = -9 --> 10011100 = 9C
 
 # Soma de negativo com negativo
-add $1, $1 # R[1] = R[1] + R[1] = -2 + (-2) = -4 --> 10010101 = 95
+add $3, $0 # R[3] = R[3] + R[0] = -9 + (-16) = -25 --> 10011100 = 9C
 
 
 ######## SUB ########
 
+# Subtração de positivo com positivo
+sub $2, $1 # R[2] = R[2] - R[1] = 14 - 7 = 7 --> 10101001 = A9
+
+# Subtração de positivo com negativo
+sub $2, $3 # R[2] = R[2] - R[3] = 7 - (-25) = 32 --> 10101011 = AB
+
 # Subtração de negativo com negativo
-sub $1, $1  # R[1] = R[1] - R[1] = -4 - (-4) = 0 --> 10100101 = A5
-                                 # Sub com um positivo e outro negativo
-                                 # Sub com um negativo e outro negativo
+sub $3, $0  # R[3] = R[3] - R[0] = -25 - (-16) = -9 --> 10101100 = AC
 and 
 or
 not
