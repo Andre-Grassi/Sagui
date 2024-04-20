a8645a8ba108f2d829c1abb5fec507ad2f9d3646
Andre Grassi <andregrassi.j@gmail.com>
2024-04-19 23:09:47 -0300

    [mod] Muda valor da posição 9 pra 1

--- soma_vet.c
+++ soma_vet.c
@@ -4,7 +4,7 @@
 
 int main () {
     // Vetores teste de 10 posições
-    int vet_1[TAM] = {0, -45, 25, 62, 2, 46, 73, 32, 11, 0};
+    int vet_1[TAM] = {0, -45, 25, 62, 2, 46, 73, 32, 11, 1};
     int vet_2[TAM] = {-5, 23, 65, 21, -24, -33, -1, 24, 63, 10};
     int vet_r[TAM];
 
